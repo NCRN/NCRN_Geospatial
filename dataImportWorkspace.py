9c829729ab73e89620626918bb3cdab1cf55ca5f
galenoett <114608937+galenoett@users.noreply.github.com>
2022-10-28 11:23:13 -0400

    Changes to workspace

--- dataImportWorkspace.py
+++ dataImportWorkspace.py
@@ -1,10 +1,16 @@
-#Download standard ZIP file
+#Download file from link
 import wget
 
-url = r'https://water.usgs.gov/GIS/dsdl/gagesII_9322_point_shapefile.zip'
+url = r'https://www.fws.gov/wetlands/Data/State-Downloads/DC_shapefile_wetlands.zip'
+wget.download(url)
 
-def bar_custom(current, total, width=80):
-    print("Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total))
+#def bar_custom(current, total, width=80):
+#    print("Downloading: %d%% [%d / %d] bytes" % (current / total * 100, current, total))
   
-wget.download(url, bar=bar_custom)
+#wget.download(url, bar=bar_custom)
 
+
+#Download ZIP file from BOX
+#import boxsdk
+#file_id = '875227512344'
+#file_content = client.file(file_id).content()
