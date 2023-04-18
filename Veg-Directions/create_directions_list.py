0816f09ddcd0307d983cb616b2f9497cc6cd4ece
galenoett <114608937+galenoett@users.noreply.github.com>
2023-04-18 15:09:20 -0400

    Updates to directions script

--- Veg-Directions/create_directions_list.py
+++ Veg-Directions/create_directions_list.py
@@ -20,6 +20,8 @@ References:
 #__status__ = "Staging"
 
 # Import statements for utilized libraries / packages
+from cmath import nan
+from tkinter.tix import ROW
 import openpyxl
 from openpyxl import load_workbook
 import docx
@@ -27,6 +29,7 @@ from docx import Document
 import docxtpl
 from docxtpl import DocxTemplate
 import os
+import pandas as pd
 
 print("### GETTING STARTED ###".format())
 
@@ -36,70 +39,57 @@ ArcGIS Toolbox script and/or command line use.
 """
 # Currently hardcoded values that may be parameterized if bundling into a tool
 
-template_path = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\Directions_Template.docx' ## Create a variable to store the full path to the Word doc template. Set prefix to the user portion of the root directory
-workbook_path = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\NCRN_Veg_Locations_Directions_Master.xlsx' ## Create a variable to store the full path to the Excel file. Set prefix to the user portion of the root directory
+main_path = r'C:\Users\goettel\DOI\NPS-NCRN-Forest Veg - Documents\General\Field_Maps_2023'
+template_path = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\Templates\NCRN\Directions_Template.docx' ## Create a variable to store the full path to the Word doc template. Set prefix to the user portion of the root directory
+__XCEL_LIBRARY = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\NCRN_Veg_Locations_Directions_Master.xlsx' ## Create a variable to store the full path to the Excel file. Set prefix to the user portion of the root directory
 
-workbook = load_workbook(workbook_path)
+workbook = load_workbook(__XCEL_LIBRARY, data_only=True)
 template = DocxTemplate(template_path)
-worksheet_areas = workbook["Dictionary_Areas"]
-worksheet_plots = workbook["Dictionary_Plots"]
 
-# dict1 = {Area_Name:[Area_Directions, Area_Warnings]} 
-for index, row in worksheet_areas.items():
+df_areas = pd.read_excel(__XCEL_LIBRARY, sheet_name='Areas Python')
+df_plots = pd.read_excel(__XCEL_LIBRARY, sheet_name='Plots', na_filter = False)
 
-
-
-## dict2 = {Area_Name:[Plot_Name, Plot_Directions, Plot_Warnings, Keys, Map_N]}
-for index, row in worksheet_plots.items():
-
-
-# Populate the template fresh, every time the Area Name changes
-if k = a: 
-
-
-#OPEN THE TEMPLATE > SAVE AS > MAKE A NEW FILE 
-
-#FILL IT OUT WITH HEADER INFO FROM dict1 
-
-#FILL OUT THE DIRECTIONS TABLE WITH ITEMS IN b, ADDING ROWS AS NEEDED FOR EACH b 
-
-	#for a, b in dict2.items(): 
-
-# This is an empty dictionary with the keys matching the first column of the excel file. For now, you assign empty values (None) and these will be filled in using the excel file in the next step.
-to_fill_in = {'Area_Name' : None,
-              'Area_Directions' : None,
-              'Area_Warnings': None,
-              'Plot_Name' : None,
-              'Plot Directions': None,
-              'Warnings' : None,
-              'Keys' : None,
-              'Map_N' : None
-              }
-
-# Set the minimum number of columns. This will be 2.
-column = 2
-
-# Perform the following code block if the colomn amount is less than the maximum column amount.
-while column <= worksheet_areas.max_column:
-
-    # Define the column index. This is a letter so you need to convert the column number to a letter (2+64) = B
-    col_index = chr(column+64)
-    row_index = 1
-    # Retrieve the values from excel document and store in dictionary you defined earlier on
-    # For each key in the dictionary, we look up the value in the excel file and store it instead of "none" in the dictionary
-    for key in to_fill_in:
-        cell = '%s%i' % (col_index, row_index)
-        to_fill_in[key] = worksheet_areas[cell].value
-        row_index += 1
-
-## Fill in all the keys defined in the word document using the dictionary.
-#   # The keys in the word document are identified by the {{}}symbols.
-#   template.render(to_fill_in)
-   
-#   # Output the file to a docx document.
-#   filename = str(to_fill_in['Company_Name']) + '_draft.docx'
-#   filled_path = os.path.join(main_path, filename)
-#   template.save(filled_path)
-#   print("Done with %s" % str(to_fill_in['Company_Name']))
-#   column += 1
-
+for index, row in df_areas.iterrows():
+    Area_Name = row['Area Name']
+    if row['Area Warnings'] == 0:
+        Area_Warnings = ""
+    else: 
+        Area_Warnings = row['Area Warnings']
+    to_fill_in_area = {
+        'Area_Name': Area_Name,
+        'Area_Directions': row['Area Directions'],
+        'Area_Warnings': Area_Warnings,
+        'Plot_Name': '{{Plot_Name}}',
+        'Plot_Directions': '{{Plot_Directions}}',
+        'Warnings': '{{Warnings}}',
+        'Keys': '{{Keys}}',
+        'Map_N': '{{Map_N}}'
+        }
+    template.render(to_fill_in_area)
+    filename = str(to_fill_in_area['Area_Name']) + ' Directions.docx'
+    filled_path = os.path.join(main_path, row['Panel'], filename)
+    template.save(filled_path)
+    print("Done with creating %s" % str(to_fill_in_area['Area_Name']))
+    for index, row in df_plots.iterrows():
+        filled_template = DocxTemplate(filled_path)
+        if row['Area/Route - Panel'] == Area_Name:
+            to_fill_in_plot = {
+                'Plot_Name': row['Plot Name'],
+                'Plot_Directions': row['Plot Directions'],
+                'Warnings': row['Plot Warnings'],
+                'Keys': row['Keys'],
+                'Map_N': row['Map #']
+                }
+            filled_template.render(to_fill_in_plot)
+            table = filled_template.tables[0]
+            row = table.add_row().cells
+            row[0].text = '{{Plot_Name}}'
+            row[1].text = '{{Plot_Directions}}'
+            row[2].text = '{{Warnings}}'
+            row[3].text = '{{Keys}}'
+            row[4].text = '{{Map_N}}'
+            filled_template.save(filled_path)
+        to_fill_in = {}
+    filled_template.render(to_fill_in)
+    filled_template.save(filled_path)
+    print("Done adding plots to %s" % str(to_fill_in_area['Area_Name']))
