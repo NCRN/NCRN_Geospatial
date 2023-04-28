e09bfa6299e801dda3de8c4112951419ea37e40a
galenoett <114608937+galenoett@users.noreply.github.com>
2023-04-28 14:46:42 -0400

    Continue to work on script to create map packet

--- Map-Packet/create_directions_list.py
+++ Map-Packet/create_directions_list.py
@@ -40,6 +40,7 @@ import sys
 from reportlab.pdfgen.canvas import Canvas
 import comtypes.client
 import time
+import fitz
 
 print("### GETTING STARTED ###".format())
 
@@ -53,6 +54,24 @@ template_path = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\Templat
 __XCEL_LIBRARY = r'C:\Users\goettel\DOI\NCRN Data Management - Geospatial\NCRN_Veg_Locations_Directions_Master.xlsx' ## Create a variable to store the full path to the Excel file. NEED TO UPDATE PREFIX TO YOUR ONEDRIVE ACCOUNT
 
 # Create some functions to be used in various places
+def Clear_Folder(folder_path):
+    """
+    Takes the path of a folder or geodatabase and deletes the files within it
+    """
+    for filename in os.listdir(folder_path):
+        fullpath_filename = os.path.join(folder_path, filename)
+        if os.path.isfile(fullpath_filename):
+            try:
+                os.remove(fullpath_filename)
+            except:
+                pass
+
+def create_blank_docx(panel, Map):
+    document = Document()
+    filename = 'blank page' + '03' + Map
+    file_path = os.path.join(__ROOT_DIR, panel, 'Directions', filename)
+    document.save(file_path)
+
 def covx_to_pdf(in_file, out_file):
     """Convert a Word .docx to PDF"""
     wdFormatPDF = 17
@@ -64,83 +83,112 @@ def covx_to_pdf(in_file, out_file):
     doc.Close()
     word.Quit()
 
-# Create a docx template for the word document downloaded earlier.
-template = DocxTemplate(template_path)
+# DOES NOT WORK
+def remove_empty_pdf_page(panel_folder, filename, page):
+    input_file = os.path.join(__ROOT_DIR, panel_folder, 'Directions', filename)
+    print(input_file)
+    output_file = input_file
+    print(output_file)
+    file_handle = fitz.open(input_file)
+    file_handle.delete_page(page)
+    file_handle.save(output_file)
+    print('worked')
 
-# Create dataframes for Areas and Plots using __XCEL_LIBRARY
-df_areas = pd.read_excel(__XCEL_LIBRARY, sheet_name='Areas Python')
-df_plots = pd.read_excel(__XCEL_LIBRARY, sheet_name='Plots', na_filter = False)
+## Create a docx template for the word document downloaded earlier.
+#template = DocxTemplate(template_path)
 
-# Loop over rows in the Areas dataframe to create the directions documents
-for index, row in df_areas.iterrows():
-    # Display '0' cells as blank
-    Area_Name = row['Area Name']
-    if row['Area Warnings'] == 0:
-        Area_Warnings = ""
-    else: 
-        Area_Warnings = row['Area Warnings']
-    # This is a dictionary with the first 3 keys matching columns in the Area dataframe. This will fill out the Area portion of the template.
-    to_fill_in_area = {
-        'Area_Name': Area_Name,
-        'Area_Directions': row['Area Directions'],
-        'Area_Warnings': Area_Warnings,
-        # The next keys refill the Plots input parameters (they are erased by the first render function)
-        'Plot_Name': '{{Plot_Name}}',
-        'Plot_Directions': '{{Plot_Directions}}',
-        'Warnings': '{{Warnings}}',
-        'Keys': '{{Keys}}',
-        'Map_N': '{{Map_N}}'
-        }
-    # Use the render function to fill in the word template based on the dictionary created.
-    template.render(to_fill_in_area)
-    filename = str(to_fill_in_area['Area_Name']) + ' Directions.docx'
-    filled_path = os.path.join(__ROOT_DIR, row['Panel'], 'Directions', filename)
-    # save the created template on the specified path
-    template.save(filled_path)
-    print("Created %s" % str(to_fill_in_area['Area_Name']))
-    # Loop over rows in the Plots dataframe to populate the directions documents
-    for index, row in df_plots.iterrows():
-        # Set the template to the document just created
-        filled_template = DocxTemplate(filled_path)
-        # Only populate document if the plot matches the area
-        if row['Area/Route - Panel'] == Area_Name:
-            # This is a dictionary with the keys matching columns in the Plots dataframe
-            to_fill_in_plot = {
-                'Plot_Name': row['Plot Name'],
-                'Plot_Directions': row['Plot Directions'],
-                'Warnings': row['Plot Warnings'],
-                'Keys': row['Keys'],
-                'Map_N': row['Map #']
-                }
-            filled_template.render(to_fill_in_plot)
-            # Create a new row in the Plots table to be filled out in the next itteration
-            table = filled_template.tables[0]
-            row = table.add_row().cells
-            row[0].text = '{{Plot_Name}}'
-            row[1].text = '{{Plot_Directions}}'
-            row[2].text = '{{Warnings}}'
-            row[3].text = '{{Keys}}'
-            row[4].text = '{{Map_N}}'
-            filled_template.save(filled_path)
-        #Delete input parameters that were left empty
-        to_fill_in = {}
-    filled_template.render(to_fill_in)
-    filled_template.save(filled_path)
-    print("Populated %s" % str(to_fill_in_area['Area_Name']))
+## Create dataframes for Areas and Plots using __XCEL_LIBRARY
+#df_areas = pd.read_excel(__XCEL_LIBRARY, sheet_name='Areas Python')
+#df_plots = pd.read_excel(__XCEL_LIBRARY, sheet_name='Plots', na_filter = False)
 
-panel_list = 'Panel_1', 'Panel_2', 'Panel_3', 'Panel_4'
-#Loop over the panel folders to convert docx to pdf
-for panel in panel_list:
-    folder_path = os.path.join(__ROOT_DIR, panel, 'Directions')
-    for filename in os.listdir(folder_path):
-        # Full path of the docx
-        in_file = os.path.join(folder_path, filename)
-        # Name of the output pdf
-        out_filename = filename.replace('.docx', '.pdf')
-        # Full path of the output pdf
-        out_file = os.path.join(folder_path, out_filename)
-        # Create an empty PDF
-        canvas = Canvas(out_file)
-        # Populate the pdf with the specified docx
-        covx_to_pdf(in_file, out_file)
-        print("Success: {0} was converted to PDF".format(filename))
+#panel_list = 'Panel_1', 'Panel_2', 'Panel_3', 'Panel_4'
+
+## Clear directions folder
+#for panel in panel_list:
+#    Clear_Folder(os.path.join(__ROOT_DIR, panel, 'Directions'))
+
+## Loop over rows in the Areas dataframe to create the directions documents
+#plots_list = []
+#for index, row in df_areas.iterrows():
+#    if row['Panel_Folder'] == 'Panel_1':
+#        # Display '0' cells as blank
+#        Area = row['Area']
+#        Panel = row['Panel']
+#        Map = row['Map']
+#        if row['Area Warnings'] == 0:
+#            Area_Warnings = ""
+#        else: 
+#            Area_Warnings = row['Area Warnings']
+#        # This is a dictionary with the first 3 keys matching columns in the Area dataframe. This will fill out the Area portion of the template.
+#        to_fill_in_area = {
+#            'Map': Map,
+#            'Area': Area,
+#            'Area_Directions': row['Area Directions'],
+#            'Area_Warnings': Area_Warnings,
+#            # The next keys refill the Plots input parameters (they are erased by the first render function)
+#            'Plot': '{{Plot_Name}}',
+#            'Plot_Directions': '{{Plot_Directions}}',
+#            'Warnings': '{{Warnings}}',
+#            'Keys': '{{Keys}}',
+#            }
+#        # Use the render function to fill in the word template based on the dictionary created.
+#        template.render(to_fill_in_area)
+#        filename = Map + ' 02 ' + row['Area'] + '.docx'
+#        #filename = str(to_fill_in_area['Area']) + ' Directions.docx'
+#        filled_path = os.path.join(__ROOT_DIR, row['Panel_Folder'], 'Directions', filename)
+#        # save the created template on the specified path
+#        template.save(filled_path)
+#        print("Created %s" % str(to_fill_in_area['Area']))
+#        # Loop over rows in the Plots dataframe to populate the directions documents
+#        for index, row in df_plots.iterrows():
+#            # Set the template to the document just created
+#            filled_template = DocxTemplate(filled_path)
+#            # Only populate document if the plot matches the area and map
+#            if (row['Area'] == Area) & (row['Panel'] == Panel) & (row['Map'] == Map):
+#                # This is a dictionary with the keys matching columns in the Plots dataframe
+#                to_fill_in_plot = {
+#                    'Plot_Name': row['Plot Name'],
+#                    'Plot_Directions': row['Plot Directions'],
+#                    'Warnings': row['Plot Warnings'],
+#                    'Keys': row['Keys'],
+#                    'Map_N': row['Map #']
+#                    }
+#                plots_list.append(row['Plot Name'])
+#                filled_template.render(to_fill_in_plot)
+#                # Create a new row in the Plots table to be filled out in the next itteration
+#                table = filled_template.tables[0]
+#                row = table.add_row().cells
+#                row[0].text = '{{Plot_Name}}'
+#                row[1].text = '{{Plot_Directions}}'
+#                row[2].text = '{{Warnings}}'
+#                row[3].text = '{{Keys}}'
+#                filled_template.save(filled_path)
+#            #Delete input parameters that were left empty
+#            to_fill_in = {}
+#        filled_template.render(to_fill_in)
+#        filled_template.save(filled_path)
+#        print("Populated %s" % str(to_fill_in_area['Area']))
+#len(plots_list)
+
+### Create blank documents as needed
+##Map1 = 
+
+##Loop over the panel folders to convert docx to pdf
+#for panel in panel_list:
+#    folder_path = os.path.join(__ROOT_DIR, panel, 'Directions')
+#    for filename in os.listdir(folder_path):
+#        if filename.endswith('.docx'):
+#            # Full path of the docx
+#            in_file = os.path.join(folder_path, filename)
+#            # Name of the output pdf
+#            out_filename = filename.replace('.docx', '.pdf')
+#            # Full path of the output pdf
+#            out_file = os.path.join(folder_path, out_filename)
+#            # Create an empty PDF
+#            canvas = Canvas(out_file)
+#            # Populate the pdf with the specified docx
+#            covx_to_pdf(in_file, out_file)
+#            print("Success: {0} was converted to PDF".format(filename))
+
+# Remove empty pages from pdf
+remove_empty_pdf_page('Panel_1', 'PRWI 02 PRWI.pdf', 2)
--- Map-Packet/create_map_packet.py
+++ Map-Packet/create_map_packet.py
@@ -27,6 +27,8 @@ References:
 # Import statements for utilized libraries / packages
 import PyPDF2
 import os
+import shutil
+
 
 print("### GETTING STARTED ###".format())
 
@@ -37,16 +39,39 @@ ArcGIS Toolbox script and/or command line use.
 # Currently hardcoded values that may be parameterized if bundling into a tool
 __ROOT_DIR = r'C:\Users\goettel\DOI\NPS-NCRN-Forest Veg - Documents\General\Field_Maps_2023' ## Set the directory path to the root directory that will be the destination for outputs. NEED TO UPDATE PREFIX TO YOUR ONEDRIVE ACCOUNT
 
-# Create some functions to be used in various places
-def create_pdf_list(folder, list_name):
+def Clear_Folder(folder_path):
+    """
+    Takes the path of a folder or geodatabase and deletes the files within it
+    """
+    try:
+        for filename in os.listdir(folder_path):
+            fullpath_filename = os.path.join(folder_path, filename)
+            if os.path.isfile(fullpath_filename):
+                os.remove(fullpath_filename)
+    except:
+        pass
+
+def add_map_suffix(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith('01.pdf'):
+            pass
+        else:
+            src = os.path.join(folder_path, filename)
+            root_ext = os.path.splitext(filename)
+            root = root_ext[0]
+            new_filename = root + ' 01' + '.pdf'
+            dst = os.path.join(folder_path, new_filename)
+            os.rename(src, dst)
+
+def create_pdf_list(folder_path, list_name):
     """
     Creates a list of PDFs
     Takes (1) the folder path to the PDFs that will be added to the list (2) what you want to name the list 
     """
-    for filename in os.listdir(folder):
+    for filename in os.listdir(folder_path):
         # Makes sure only PDFs are added to the list
         if filename.endswith('.pdf'):
-            list_name.append(os.path.join(folder, filename))
+            list_name.append(os.path.join(folder_path, filename))
     # Sorts the list alphabetically
     list_name.sort(key = str.lower)
 
@@ -63,36 +88,65 @@ def merge_pdfs(pdfiles, name):
     pdfFile.close()
     pdfMerge.write(os.path.join(__ROOT_DIR, 'Packets', name))
 
+def copy_paste_files(source_folder, destination_folder):
+    for filename in os.listdir(source_folder):
+        if filename.endswith('.pdf'):
+            # construct full file path
+            source = os.path.join(source_folder, filename)
+            destination = os.path.join(destination_folder, filename)
+            # copy only files
+            if os.path.isfile(source):
+                shutil.copy(source, destination)
+                print('copied', filename)
+
+panel_list = 'Panel_1', 'Panel_2', 'Panel_3', 'Panel_4'
+
+for panel in panel_list:
+    folder_path = os.path.join(__ROOT_DIR, panel, 'Maps')
+    add_map_suffix(folder_path)
+
+# Clear maps/directions folder
+for panel in panel_list:
+    Clear_Folder(os.path.join(__ROOT_DIR, panel, 'Maps_Directions_Combined'))
+
 # Set the folder path to the Legend pdf
 folder_legend = os.path.join(__ROOT_DIR, 'Legend')
-# Create an empty list to populate with the Legend pdf
+# Create a list to populate with the Legend pdf
 pdfiles_legend = []
 create_pdf_list(folder_legend, pdfiles_legend)
+
+
+# Eventually refactor so that it is a loop
+#for panel in panel_list = 
 
 # Create the Panel 1 packet
 # Set the folder path to the Cover page pdf
 folder_cover_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Cover')
-# Create an empty list to populate with the Cover page pdf
+# Create a list to populate with the Cover page pdf
 pdfiles_cover_panel1 = []
 create_pdf_list(folder_cover_panel1, pdfiles_cover_panel1)
-# Set the folder path to the Directions pdfs
-folder_directions_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Directions')
-# Create an empty list to populate with the Directions pdfs
-pdfiles_directions_panel1 = []
-create_pdf_list(folder_directions_panel1, pdfiles_directions_panel1)
+
 # Set the folder path to the Overview map pdf
 folder_overview_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Overview')
-# Create an empty list to populate with the Overview map pdf
+# Create a list to populate with the Overview map pdf
 pdfiles_overvew_panel1 = []
 create_pdf_list(folder_overview_panel1, pdfiles_overvew_panel1)
-# Set the folder path to the Maps pdfs
+
 folder_maps_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Maps')
-# Create an empty list to populate with the Map pdfs
-pdfiles_maps_panel1 = []
-create_pdf_list(folder_maps_panel1, pdfiles_maps_panel1)
+folder_directions_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Directions')
+folder_maps_directions_panel1 = os.path.join(__ROOT_DIR, 'Panel_1', 'Maps_Directions_Combined')
+
+copy_paste_files(folder_maps_panel1, folder_maps_directions_panel1)
+copy_paste_files(folder_directions_panel1, folder_maps_directions_panel1)
+
+# Create a list to populate with the Maps and Directions
+pdfiles_maps_directions_panel1 = []
+create_pdf_list(folder_maps_directions_panel1, pdfiles_maps_directions_panel1)
+# Sort ABC
+pdfiles_maps_directions_panel1.sort(key = str.lower)
 
 # Create a giant list with everything you want to include in the merged PDF
-pdfiles_panel1 = pdfiles_cover_panel1 + pdfiles_directions_panel1 + pdfiles_overvew_panel1 + pdfiles_maps_panel1 + pdfiles_legend
+pdfiles_panel1 = pdfiles_cover_panel1 + pdfiles_overvew_panel1 + pdfiles_maps_directions_panel1 + pdfiles_legend
 packet1 = 'Packet_Panel_1.pdf'
 merge_pdfs(pdfiles_panel1, packet1)
 print("{0} was created".format(packet1))
